fd62c6994223716d4e201a79277a96c01441d7a7
Hi-kue <lofi.audit@gmail.com>
2025-01-23 11:44:00 -0500

    add: /crud, /security

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,5 +1,13 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase(process.env.PRIVATE_POCKETBASE_URL);
+
+if (!process.env.PRIVATE_POCKETBASE_URL || process.env.PRIVATE_POCKETBASE_URL === "") {
+    throw new Error("PRIVATE_POCKETBASE_UR is not set.");
+    
+} else {
+    const pb = new PocketBase(process.env.PRIVATE_POCKETBASE_URL);
+    pb.autoCancellation(false);
+    return pb;
+}
 
 export default pb;
